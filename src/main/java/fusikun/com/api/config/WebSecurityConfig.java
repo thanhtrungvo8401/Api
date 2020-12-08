affaabc69031232a8dc06734c78ee1cc028ffc30
trung8401 <thanhtrungvo8401@gmail.com>
2020-12-08 11:50:21 +0700

    02 - handle create authorization with JWT, and config spring-security

--- src/main/java/fusikun/com/api/config/WebSecurityConfig.java
+++ src/main/java/fusikun/com/api/config/WebSecurityConfig.java
@@ -3,10 +3,13 @@ package fusikun.com.api.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -28,11 +31,14 @@ import fusikun.com.api.utils.JwtAuthenticationEntryPoint;
 
 @Configuration
 @EnableWebSecurity
-//@EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private JwtUserDetailsService jwtUserDetailsService; 
 	
+	@Autowired
+	private JwtRequestFilter jwtRequestFilter;
+
 	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
@@ -51,17 +57,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable()
-			.authorizeRequests().antMatchers("/authenticate").permitAll()
-			.anyRequest().authenticated();
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers("/authenticate").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 	}
+	
 	/**
 	 
 	@Autowired
 	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-	@Autowired
-	private JwtRequestFilter jwtRequestFilter;
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
--- src/main/java/fusikun/com/api/requestFilters/JwtRequestFilter.java
+++ src/main/java/fusikun/com/api/requestFilters/JwtRequestFilter.java
@@ -1,4 +1,4 @@
-package fusikun.com.api.jwtUtils;
+package fusikun.com.api.requestFilters;
 
 import java.io.IOException;
 
@@ -16,7 +16,8 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import fusikun.com.api.service.JwtUserDetailsService;
-import io.jsonwebtoken.ExpiredJwtException;
+import fusikun.com.api.utils.AuthContants;
+import fusikun.com.api.utils.JwtTokenUtil;
 
 @Component
 public class JwtRequestFilter extends OncePerRequestFilter {
@@ -28,42 +29,61 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 	private JwtTokenUtil jwtTokenUtil;
 
 	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
 			throws ServletException, IOException {
-		final String requestTokenHeader = request.getHeader("Authorization");
+		final String authorizationHeader = request.getHeader(AuthContants.AUTHORIZATION);
 		String username = null;
-		String jwtToken = null;
-		// JWT Token is in the form "Bearer Token", Remove Bearer word and get:
-		// only the Token:
-		if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer")) {
-			jwtToken = requestTokenHeader.substring(7);
-			try {
-				username = jwtTokenUtil.getUserNameFromToken(jwtToken);
-			} catch (IllegalArgumentException e) {
-				System.out.println("Unable to get JWT Token");
-			} catch (ExpiredJwtException e) {
-				System.out.println("JWT Token has expired");
-			}
-		} else {
-			logger.warn("JWT Token does not begin with Bearer String");
+		String jwt = null;
+		if (authorizationHeader != null && authorizationHeader.startsWith(AuthContants.BEARER)) {
+			jwt = authorizationHeader.substring(AuthContants.BEARER_INDEX);
+			username = jwtTokenUtil.getUserNameFromToken(jwt);
 		}
-
+		
 		if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			UserDetails userDetails = this.jwtUserDetailsService.loadUserByUsername(username);
-			// if token is valid configure Spring Security to manually set:
-			// authentication
-			if (jwtTokenUtil.validateToken(jwtToken, userDetails)) {
+			UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(username);
+			if (jwtTokenUtil.validateToken(jwt, userDetails)) {
 				UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(
 						userDetails, null, userDetails.getAuthorities());
+
 				usernamePasswordAuthenticationToken
 						.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-				// After setting the Authentication in the context, we specify
-				// that the current user is authenticated. So it passes the
-				// Spring Security Configurations successfully
 				SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
 			}
 		}
-		filterChain.doFilter(request, response);
+		chain.doFilter(request, response);
 	}
 
+	/**
+	 * @Override protected void doFilterInternal(HttpServletRequest request,
+	 *           HttpServletResponse response, FilterChain filterChain) throws
+	 *           ServletException, IOException { final String requestTokenHeader =
+	 *           request.getHeader("Authorization"); String username = null; String
+	 *           jwtToken = null; // JWT Token is in the form "Bearer Token", Remove
+	 *           Bearer word and get: // only the Token: if (requestTokenHeader !=
+	 *           null && requestTokenHeader.startsWith("Bearer")) { jwtToken =
+	 *           requestTokenHeader.substring(7); try { username =
+	 *           jwtTokenUtil.getUserNameFromToken(jwtToken); } catch
+	 *           (IllegalArgumentException e) { System.out.println("Unable to get
+	 *           JWT Token"); } catch (ExpiredJwtException e) {
+	 *           System.out.println("JWT Token has expired"); } } else {
+	 *           logger.warn("JWT Token does not begin with Bearer String"); }
+	 * 
+	 *           if (username != null &&
+	 *           SecurityContextHolder.getContext().getAuthentication() == null) {
+	 *           UserDetails userDetails =
+	 *           this.jwtUserDetailsService.loadUserByUsername(username); // if
+	 *           token is valid configure Spring Security to manually set: //
+	 *           authentication if (jwtTokenUtil.validateToken(jwtToken,
+	 *           userDetails)) { UsernamePasswordAuthenticationToken
+	 *           usernamePasswordAuthenticationToken = new
+	 *           UsernamePasswordAuthenticationToken( userDetails, null,
+	 *           userDetails.getAuthorities()); usernamePasswordAuthenticationToken
+	 *           .setDetails(new
+	 *           WebAuthenticationDetailsSource().buildDetails(request)); // After
+	 *           setting the Authentication in the context, we specify // that the
+	 *           current user is authenticated. So it passes the // Spring Security
+	 *           Configurations successfully
+	 *           SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+	 *           } } filterChain.doFilter(request, response); }
+	 */
 }
