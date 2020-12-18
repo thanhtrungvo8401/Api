dcf10c90b9b200019cff2e1387d5a9beb359e3fd
trung8401 <thanhtrungvo8401@gmail.com>
2020-12-18 21:05:44 +0700

    03 -- create /menu-actions/create success

--- src/main/java/fusikun/com/api/controller/MenuController.java
+++ src/main/java/fusikun/com/api/controller/MenuController.java
@@ -4,7 +4,6 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,13 +14,13 @@ import fusikun.com.api.dto.MenuResponse;
 import fusikun.com.api.exceptionHandlers.Customize_MethodArgumentNotValidException;
 import fusikun.com.api.model.Menu;
 import fusikun.com.api.service.MenuServiceImpl;
-import fusikun.com.api.validator.MenuValidator;
+import fusikun.com.api.validator.MenuDataValidate;
 
 @RestController
 public class MenuController {
 
 	@Autowired
-	MenuValidator menuValidator;
+	MenuDataValidate menuDataValidate;
 	
 	@Autowired
 	MenuServiceImpl menuService;
@@ -34,12 +33,7 @@ public class MenuController {
 	@PostMapping("/menu-actions/create")
 	public ResponseEntity<Object> createMenuActions(@Valid @RequestBody MenuRequest menuRequest)
 			throws Customize_MethodArgumentNotValidException {
-		// Validation After pass Spring-Validation:
-		BindException errors = new BindException(menuRequest, MenuRequest.class.getName());
-		menuValidator.validate(menuRequest, errors);
-		if (errors.hasErrors()) {
-			throw new Customize_MethodArgumentNotValidException(errors.getBindingResult());
-		}
+		menuDataValidate.validate(menuRequest);
 		Menu menu = menuRequest.getMenu();
 		Menu savedMenu = menuService.save(menu);
 		MenuResponse menuResponse = new MenuResponse(savedMenu);
