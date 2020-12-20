31b02032eb012e513e0bf4bd35dc90f2710b51f9
trung8401 <thanhtrungvo8401@gmail.com>
2020-12-20 19:02:28 +0700

    03 -- handle add Dynamic search config for project

--- src/main/java/fusikun/com/api/dao/MenuRepository.java
+++ src/main/java/fusikun/com/api/dao/MenuRepository.java
@@ -3,12 +3,13 @@ package fusikun.com.api.dao;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import fusikun.com.api.model.Menu;
 
 @Repository
-public interface MenuRepository extends JpaRepository<Menu, Long> {
+public interface MenuRepository extends JpaRepository<Menu, Long>, JpaSpecificationExecutor<Menu> {
 	Optional<Menu> findByName(String name);
 
 	Integer countByName(String name);
