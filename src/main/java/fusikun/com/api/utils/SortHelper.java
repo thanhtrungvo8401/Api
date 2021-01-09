2272a2587cb94c46676dd6bb5d86215af2a5524e
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-09 19:36:45 +0700

    06 -- handle rename 'direction' to 'order'

--- src/main/java/fusikun/com/api/utils/SortHelper.java
+++ src/main/java/fusikun/com/api/utils/SortHelper.java
@@ -12,7 +12,7 @@ public class SortHelper {
 		Integer limit = (StringUtils.hasText(sorts.get("limit"))) ? Integer.parseInt(sorts.get("limit")) : 10;
 		Integer page = (StringUtils.hasText(sorts.get("page"))) ? Integer.parseInt(sorts.get("page")) : 0;
 		String sortBy = (StringUtils.hasText(sorts.get("sortBy"))) ? sorts.get("sortBy") : "id";
-		Direction direction = (sorts.get("direction") == "DESC") ? Direction.DESC : Direction.ASC;
+		Direction direction = (sorts.get("order") == "DESC") ? Direction.DESC : Direction.ASC;
 		return PageRequest.of(page, limit, direction, sortBy);
 	}
 }
