aaf9d7fd8807655b8ece6f478212b44dd30c4f78
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-15 20:30:03 +0700

    11 -- handle create Voca Entity

--- src/main/java/fusikun/com/api/model/study/Voca.java
+++ src/main/java/fusikun/com/api/model/study/Voca.java
@@ -1,12 +1,28 @@
 package fusikun.com.api.model.study;
 
 import java.util.Date;
+import java.util.UUID;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "voca")
 public class Voca {
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "uuid2")
+	@Column(name = "id", columnDefinition = "BINARY(16)")
+	private UUID id;
+
 	private String voca;
 	private String meaning;
 	private String kanji;
