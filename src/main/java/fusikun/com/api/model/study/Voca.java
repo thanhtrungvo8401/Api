a39ab7e195630edc7845e8a85b692f0feea7ff16
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-15 20:22:01 +0700

    11 -- handle create Voca Entity

--- src/main/java/fusikun/com/api/model/study/Voca.java
+++ src/main/java/fusikun/com/api/model/study/Voca.java
@@ -1,8 +1,85 @@
 package fusikun.com.api.model.study;
 
+import java.util.Date;
+
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 public class Voca {
-//	String voca;
-//	String meaning;
-//	String kanji;
-//	String sentence;
+	private String voca;
+	private String meaning;
+	private String kanji;
+	private String sentence;
+	private Boolean isActive;
+	private Date createdDate;
+	private Date updatedDate;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "setId", nullable = false)
+	private SetVoca setVoca;
+
+	public String getVoca() {
+		return voca;
+	}
+
+	public void setVoca(String voca) {
+		this.voca = voca;
+	}
+
+	public String getMeaning() {
+		return meaning;
+	}
+
+	public void setMeaning(String meaning) {
+		this.meaning = meaning;
+	}
+
+	public String getKanji() {
+		return kanji;
+	}
+
+	public void setKanji(String kanji) {
+		this.kanji = kanji;
+	}
+
+	public String getSentence() {
+		return sentence;
+	}
+
+	public void setSentence(String sentence) {
+		this.sentence = sentence;
+	}
+
+	public SetVoca getSetVoca() {
+		return setVoca;
+	}
+
+	public void setSetVoca(SetVoca setVoca) {
+		this.setVoca = setVoca;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
 }
