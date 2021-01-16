0329cc23f50c0b08c9d7e67c15340f1bf5413bbe
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-16 16:11:20 +0700

    11 -- handle create User Entity

--- src/main/java/fusikun/com/api/model/app/User.java
+++ src/main/java/fusikun/com/api/model/app/User.java
@@ -1,6 +1,7 @@
 package fusikun.com.api.model.app;
 
 import java.util.Date;
+import java.util.List;
 import java.util.UUID;
 
 import javax.persistence.Column;
@@ -9,10 +10,14 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import org.hibernate.annotations.GenericGenerator;
+
+import fusikun.com.api.model.study.Center;
+import fusikun.com.api.model.study.Room;
 
 @Entity
 @Table(name = "user")
@@ -20,7 +25,7 @@ public class User {
 	@Id
 	@GeneratedValue(generator = "uuid2")
 	@GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(name = "id", columnDefinition = "BINARY(16)")
+	@Column(name = "id", columnDefinition = "BINARY(16)")
 	private UUID id;
 
 	private String password;
@@ -38,6 +43,13 @@ public class User {
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name = "roleId", nullable = false)
 	private Role role;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "centerId", nullable = true)
+	private Center center;
+
+	@ManyToMany(mappedBy = "members", fetch = FetchType.LAZY)
+	private List<Room> rooms;
 
 	public Boolean getIsActive() {
 		return isActive;
