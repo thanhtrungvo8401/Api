4f4b00f02f99fb0363ccb4659c3482a16f475d80
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-16 15:55:57 +0700

    11 -- handle create CENTER Entity

--- src/main/java/fusikun/com/api/model/study/Center.java
+++ src/main/java/fusikun/com/api/model/study/Center.java
@@ -1,5 +1,98 @@
 package fusikun.com.api.model.study;
 
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import fusikun.com.api.model.app.User;
+
 public class Center {
+
+	@Id
+	@GeneratedValue(generator = "uuid2")
+	@GenericGenerator(name = "uuid2", strategy = "uuid2")
+	@Column(name = "id", columnDefinition = "BINARY(16)")
+	private UUID id;
+
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "center")
+	private List<User> members;
+
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "adminId", nullable = false)
+	private User admin;
+
+	private String centerName;
+
+	private Boolean isActive;
+
+	private Date createdDate;
+
+	private Date updatedDate;
+
+	public User getAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(User admin) {
+		this.admin = admin;
+	}
+
+	public String getCenterName() {
+		return centerName;
+	}
+
+	public void setCenterName(String centerName) {
+		this.centerName = centerName;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	public List<User> getMembers() {
+		return members;
+	}
+
+	public void setMembers(List<User> members) {
+		this.members = members;
+	}
 
 }
