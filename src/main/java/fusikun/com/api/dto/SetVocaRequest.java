598266f038c1bbef4a155465e81c594a63f97154
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-17 11:25:06 +0700

    11 -- create SetVoca, Voca Request, Response for Page

---
+++ src/main/java/fusikun/com/api/dto/SetVocaRequest.java
@@ -0,0 +1,66 @@
+package fusikun.com.api.dto;
+
+import java.util.UUID;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import fusikun.com.api.model.app.User;
+import fusikun.com.api.model.study.SetVoca;
+import fusikun.com.api.utils.ConstantErrorCodes;
+
+public class SetVocaRequest {
+	private UUID id;
+
+	private final Integer maxVoca = 10;
+
+	@NotNull(message = ConstantErrorCodes.NOT_NULL)
+	@NotEmpty(message = ConstantErrorCodes.NOT_EMPTY)
+	@NotBlank(message = ConstantErrorCodes.NOT_BLANK)
+	private String setName;
+
+	@NotNull(message = ConstantErrorCodes.NOT_NULL)
+	private UUID authorId;
+
+	public SetVoca getSetVoca() {
+		SetVoca setVoca = new SetVoca();
+		setVoca.setId(id);
+		setVoca.setMaxVoca(maxVoca);
+		setVoca.setSetName(setName);
+
+		User author = new User();
+		author.setId(authorId);
+		setVoca.setAuthor(author);
+		return setVoca;
+	}
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getSetName() {
+		return setName;
+	}
+
+	public void setSetName(String setName) {
+		this.setName = setName;
+	}
+
+	public UUID getAuthorId() {
+		return authorId;
+	}
+
+	public void setAuthorId(UUID authorId) {
+		this.authorId = authorId;
+	}
+
+	public Integer getMaxVoca() {
+		return maxVoca;
+	}
+
+}
---
+++ src/main/java/fusikun/com/api/dto/VocaRequest.java
@@ -0,0 +1,84 @@
+package fusikun.com.api.dto;
+
+import java.util.UUID;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import fusikun.com.api.utils.ConstantErrorCodes;
+
+public class VocaRequest {
+	private UUID id;
+
+	@Size(max = 50, message = ConstantErrorCodes.NOT_OVER_50_LETTER)
+	@NotNull(message = ConstantErrorCodes.NOT_NULL)
+	@NotEmpty(message = ConstantErrorCodes.NOT_EMPTY)
+	@NotBlank(message = ConstantErrorCodes.NOT_BLANK)
+	private String voca;
+
+	@Size(max = 50, message = ConstantErrorCodes.NOT_OVER_50_LETTER)
+	@NotNull(message = ConstantErrorCodes.NOT_NULL)
+	@NotEmpty(message = ConstantErrorCodes.NOT_EMPTY)
+	@NotBlank(message = ConstantErrorCodes.NOT_BLANK)
+	private String meaning;
+
+	@Size(max = 50, message = ConstantErrorCodes.NOT_OVER_50_LETTER)
+	private String kanji;
+
+	@Size(max = 255, message = ConstantErrorCodes.NOT_OVER_255_LETTER)
+	private String sentence;
+
+	@NotNull(message = ConstantErrorCodes.NOT_NULL)
+	private UUID setId;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getVoca() {
+		return voca;
+	}
+
+	public void setVoca(String voca) {
+		this.voca = voca;
+	}
+
+	public String getMeaning() {
+		return meaning;
+	}
+
+	public void setMeaning(String meaning) {
+		this.meaning = meaning;
+	}
+
+	public String getKanji() {
+		return kanji;
+	}
+
+	public void setKanji(String kanji) {
+		this.kanji = kanji;
+	}
+
+	public String getSentence() {
+		return sentence;
+	}
+
+	public void setSentence(String sentence) {
+		this.sentence = sentence;
+	}
+
+	public UUID getSetId() {
+		return setId;
+	}
+
+	public void setSetId(UUID setId) {
+		this.setId = setId;
+	}
+
+}
