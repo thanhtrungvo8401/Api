2ae3635478f0b042e91d83c0d48bacd609749bcf
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-19 09:43:50 +0700

    API convention for USER-UPDATE

--- src/main/java/fusikun/com/api/controller/UserController.java
+++ src/main/java/fusikun/com/api/controller/UserController.java
@@ -14,9 +14,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -73,7 +73,7 @@ public class UserController {
 		return ResponseEntity.ok(new UserResponse(user));
 	}
 
-	@PatchMapping("/users/{id}/update")
+	@PutMapping("/users/{id}")
 	public ResponseEntity<Object> handleUpdateUserById(@Valid @RequestBody UserRequest userRequest,
 			@PathVariable UUID id) throws NotFoundException, Ex_MethodArgumentNotValidException {
 		// Validate data: (validate email, if password was posted => also validate)
