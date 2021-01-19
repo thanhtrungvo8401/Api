a51bb8d347598a1db92dba4f0dbab81ff1d2a02c
trung8401 <thanhtrungvo8401@gmail.com>
2021-01-19 09:09:38 +0700

    Lazy load for user -> role

--- src/main/java/fusikun/com/api/model/User.java
+++ src/main/java/fusikun/com/api/model/User.java
@@ -35,7 +35,7 @@ public class User {
 
 	private Date updatedDate;
 
-	@ManyToOne(fetch = FetchType.EAGER)
+	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "roleId", nullable = false)
 	private Role role;
 
