5c80e0321a0d9d63d563d8721e0b918dc1f4d0fc
trung8401 <thanhtrungvo8401@gmail.com>
2021-04-01 20:43:37 +0700

    007 -- handle optimize code

--- src/main/java/fusikun/com/api/service/VocaService.java
+++ src/main/java/fusikun/com/api/service/VocaService.java
@@ -20,64 +20,69 @@ import fusikun.com.api.enums.SearchOperator;
 
 @Service
 public class VocaService {
-	@Autowired
-	VocaRepository vocaRepository;
+    @Autowired
+    VocaRepository vocaRepository;
 
-	@Autowired SetVocaService setVocaService;
+    @Autowired
+    SetVocaService setVocaService;
 
-	public Voca findById(UUID id) {
-		Optional<Voca> optVoca = vocaRepository.findById(id);
-		return optVoca.orElse(null);
-	}
+    public Voca findById(UUID id) {
+        Optional<Voca> optVoca = vocaRepository.findById(id);
+        return optVoca.orElse(null);
+    }
 
-	public Voca save(Voca entity) {
-		if (entity.getId() == null) {
-			entity.setCreatedDate(new Date());
-		}
-		entity.setUpdatedDate(new Date());
-		if (entity.getIsActive() == null) {
-			entity.setIsActive(true);
-		}
-		return vocaRepository.save(entity);
-	}
+    public Voca save(Voca entity) {
+        if (entity.getId() == null) {
+            entity.setCreatedDate(new Date());
+        }
+        entity.setUpdatedDate(new Date());
+        if (entity.getIsActive() == null) {
+            entity.setIsActive(true);
+        }
+        return vocaRepository.save(entity);
+    }
 
-	public List<Voca> findAll(Specification_Voca specification, Pageable pageable) {
-		Page<Voca> page = vocaRepository.findAll(specification, pageable);
-		if (page.hasContent())
-			return page.getContent();
-		return new ArrayList<>();
+    public List<Voca> findAll(Specification_Voca specification, Pageable pageable) {
+        Page<Voca> page = vocaRepository.findAll(specification, pageable);
+        if (page.hasContent())
+            return page.getContent();
+        return new ArrayList<>();
 
-	}
+    }
 
-	public Long count(Specification_Voca specification) {
-		return vocaRepository.count(specification);
-	}
+    public List<Voca> findAll(Specification_Voca specification) {
+        return vocaRepository.findAll(specification);
+    }
 
-	public Long countVocaBySetVocaId(UUID setVocaId) {
-		Specification_Voca specification = getVocaSpecification(setVocaId);
-		return count(specification);
-	}
+    public Long count(Specification_Voca specification) {
+        return vocaRepository.count(specification);
+    }
 
-	public void deleteById(UUID id) {
-		vocaRepository.deleteById(id);
-	}
+    public Long countVocaBySetVocaId(UUID setVocaId) {
+        Specification_Voca specification = getVocaSpecification(setVocaId);
+        return count(specification);
+    }
 
-	public Voca create(Voca entity) {
-		SetVoca setVoca = setVocaService.findById(entity.getSetVoca().getId());
-		setVoca.increaseVoca();
-		setVocaService.save(setVoca);
-		return save(entity);
-	}
+    public void deleteById(UUID id) {
+        vocaRepository.deleteById(id);
+    }
 
-	public void delete(Voca voca) {
-		vocaRepository.delete(voca);
-	}
+    public Voca create(Voca entity) {
+        SetVoca setVoca = setVocaService.findById(entity.getSetVoca().getId());
+        setVoca.increaseVoca();
+        setVocaService.save(setVoca);
+        return save(entity);
+    }
 
-	private Specification_Voca getVocaSpecification(UUID setVocaId) {
-		Specification_Voca specification = new Specification_Voca();
-		SetVoca setVoca = new SetVoca();
-		setVoca.setId(setVocaId);
-		specification.add(new _SearchCriteria("setVoca", SearchOperator.EQUAL, setVoca));
-		return specification;
-	}
+    public void delete(Voca voca) {
+        vocaRepository.delete(voca);
+    }
+
+    private Specification_Voca getVocaSpecification(UUID setVocaId) {
+        Specification_Voca specification = new Specification_Voca();
+        SetVoca setVoca = new SetVoca();
+        setVoca.setId(setVocaId);
+        specification.add(new _SearchCriteria("setVoca", SearchOperator.EQUAL, setVoca));
+        return specification;
+    }
 }
