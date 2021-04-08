d5be8cd22bd4e9489eb1652264cb39138f47c192
trung8401 <thanhtrungvo8401@gmail.com>
2021-04-08 15:15:30 +0700

    007 -- optimize code in MenuController

--- src/main/java/fusikun/com/api/controller/MenuController.java
+++ src/main/java/fusikun/com/api/controller/MenuController.java
@@ -1,104 +1,31 @@
 package fusikun.com.api.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 import fusikun.com.api.dtoRES.ObjectsManagementList;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import fusikun.com.api.dtoREQ.MenuRequest;
-import fusikun.com.api.dtoRES.MenuResponse;
-import fusikun.com.api.enums.UrlEnpointEnums;
 import fusikun.com.api.exceptionHandlers.Ex_MethodArgumentNotValidException;
-import fusikun.com.api.model.app.Menu;
 import fusikun.com.api.service.MenuService;
-import fusikun.com.api.utils.Constant;
-import fusikun.com.api.validator.MenuDataValidate;
 
 @RestController
 @RequestMapping("/api/v1")
 public class MenuController {
 
     @Autowired
-    MenuDataValidate menuDataValidate;
-
-    @Autowired
     MenuService menuService;
 
     @GetMapping("/menu-actions")
-    public ResponseEntity<Object> getMenuActions() {
-        List<Menu> listMenus = menuService.findAll();
-        Long total = menuService.countActionMenus();
-        List<MenuResponse> listResponse = listMenus.stream()
-                .map(MenuResponse::new)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(new ListMenuActionsResponse(listResponse, total));
-    }
-
-    @GetMapping("/menu-actions/count")
-    public ResponseEntity<Object> getMenuActionsCount() {
-        Long total = menuService.countActionMenus();
-        return ResponseEntity.ok(total);
+    public ResponseEntity<ObjectsManagementList> getMenuActions() {
+        return ResponseEntity.ok(menuService._findAllMenuActions());
     }
 
     @GetMapping("/menu-actions/generate")
     public ResponseEntity<Object> generateActionsMenu() throws Ex_MethodArgumentNotValidException {
-        deleteMenuActionsNotInList();
-        for (UrlEnpointEnums enpoint : UrlEnpointEnums.values()) {
-            MenuActionGenerateUitls(enpoint);
-        }
+        menuService._generateActionsMenu();
         return ResponseEntity.status(201).body("SUCCESS");
-    }
-
-    private void MenuActionGenerateUitls(UrlEnpointEnums enpoint) throws Ex_MethodArgumentNotValidException {
-        String enpointName = enpoint.toString();
-        String enpointUrl = enpoint.getUrl();
-        String enpointRegex = enpoint.getRegex();
-        String enpointMethod = enpoint.getMethod();
-        MenuRequest menuRequest = new MenuRequest();
-        menuRequest.setName(enpointName);
-        menuRequest.setUrl(enpointUrl);
-        menuRequest.setRegex(enpointRegex);
-        menuRequest.setMethod(enpointMethod);
-        if (!menuService.existsByName(enpointName)) {
-            if (enpointName.contains(Constant.MENU_ADDRESS_DEVIDE)) {
-                String parentName = enpointName.split(Constant.MENU_ADDRESS_DEVIDE)[0];
-                Menu parentMenu = menuService.findByName(parentName);
-                menuRequest.setParentId(parentMenu.getId());
-            }
-            createMenuActions(menuRequest);
-        }
-    }
-
-    private void createMenuActions(MenuRequest menuRequest) throws Ex_MethodArgumentNotValidException {
-        menuDataValidate.validate(menuRequest);
-        Menu menu = menuRequest.getMenu();
-        Menu savedMenu = menuService.save(menu);
-        System.out.println(savedMenu.toString());
-    }
-
-    private void deleteMenuActionsNotInList() {
-        List<String> list = new ArrayList<>();
-        for (UrlEnpointEnums enpoint : UrlEnpointEnums.values()) {
-            list.add(enpoint.toString());
-        }
-        menuService.deleteMenuHasNameNotInList(list);
-    }
-
-    @Getter
-    @Setter
-    @NoArgsConstructor
-    private class ListMenuActionsResponse extends ObjectsManagementList<MenuResponse> {
-        ListMenuActionsResponse(List<MenuResponse> list, Long total) {
-            super(list, total);
-        }
     }
 }
--- src/main/java/fusikun/com/api/service/MenuService.java
+++ src/main/java/fusikun/com/api/service/MenuService.java
@@ -1,10 +1,18 @@
 package fusikun.com.api.service;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import fusikun.com.api.dtoREQ.MenuRequest;
+import fusikun.com.api.dtoRES.MenuResponse;
+import fusikun.com.api.dtoRES.ObjectsManagementList;
+import fusikun.com.api.enums.UrlEnpointEnums;
+import fusikun.com.api.exceptionHandlers.Ex_MethodArgumentNotValidException;
+import fusikun.com.api.utils.Constant;
+import fusikun.com.api.validator.MenuDataValidate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,74 +26,105 @@ import fusikun.com.api.enums.SearchOperator;
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class MenuService {
-	@Autowired
-	MenuRepository menuRepository;
+    @Autowired
+    MenuRepository menuRepository;
 
-	public Menu findById(UUID id) {
-		Optional<Menu> optMenu = menuRepository.findById(id);
-		if (optMenu.isPresent()) {
-			return optMenu.get();
-		} else
-			return null;
-	}
+    @Autowired
+    MenuDataValidate menuDataValidate;
 
-	public Menu findByName(String name) {
-		List<Menu> list = menuRepository.findByName(name);
-		if (!list.isEmpty())
-			return list.get(0);
-		else
-			return null;
-	}
+    public Menu findById(UUID id) {
+        Optional<Menu> optMenu = menuRepository.findById(id);
+        return optMenu.orElse(null);
+    }
 
-	public Menu save(Menu entity) {
-		if (entity.getId() == null) {
-			entity.setCreatedDate(new Date());
-		}
-		entity.setUpdatedDate(new Date());
-		if (entity.getIsActive() == null) {
-			entity.setIsActive(true);
-		}
-		return menuRepository.save(entity);
-	}
+    public Menu findByName(String name) {
+        List<Menu> list = menuRepository.findByName(name);
+        if (!list.isEmpty())
+            return list.get(0);
+        else
+            return null;
+    }
 
-	public List<Menu> findAll() {
-		return menuRepository.findAll();
-	}
+    public Menu save(Menu entity) {
+        if (entity.getId() == null) {
+            entity.setCreatedDate(new Date());
+        }
+        entity.setUpdatedDate(new Date());
+        if (entity.getIsActive() == null) {
+            entity.setIsActive(true);
+        }
+        return menuRepository.save(entity);
+    }
 
-	public List<Menu> findNotMappedMenus(List<String> listName) {
-		Specification_Menu menuSpecification = new Specification_Menu();
-		menuSpecification.add(new _SearchCriteria("name", SearchOperator.NOT_IN, listName));
-		return menuRepository.findAll(menuSpecification);
-	}
+    public List<Menu> findAll() {
+        return menuRepository.findAll();
+    }
 
-	public List<Menu> findAllParentMenus() {
-		return menuRepository.findAllParentMenu();
-	}
+    public List<Menu> findNotMappedMenus(List<String> listName) {
+        Specification_Menu menuSpecification = new Specification_Menu();
+        menuSpecification.add(new _SearchCriteria("name", SearchOperator.NOT_IN, listName));
+        return menuRepository.findAll(menuSpecification);
+    }
 
-	public Boolean existsById(UUID id) {
-		return menuRepository.existsById(id);
-	}
+    public Boolean existsById(UUID id) {
+        return menuRepository.existsById(id);
+    }
 
-	public Boolean existsByName(String name) {
-		return this.countByName(name) > 0;
-	}
+    public Boolean existsByName(String name) {
+        return this.countByName(name) > 0;
+    }
 
-	public Long countActionMenus() {
-		return menuRepository.count();
-	}
+    public Integer countByName(String name) {
+        return menuRepository.countByName(name);
+    }
 
-	public Integer countByName(String name) {
-		return menuRepository.countByName(name);
-	}
+    public MenuActionsMangement _findAllMenuActions() {
+        List<Menu> menus = menuRepository.findAll();
+        Long total = menuRepository.count();
+        List<MenuResponse> menuResponses = menus.stream().map(MenuResponse::new).collect(Collectors.toList());
+        return new MenuActionsMangement(menuResponses, total);
+    }
 
-	public void deleteById(UUID id) {
-		menuRepository.deleteById(id);
-	}
+    public void _generateActionsMenu() throws Ex_MethodArgumentNotValidException {
+        // Delete
+        List<String> listName = new ArrayList<>();
+        for (UrlEnpointEnums enpoint : UrlEnpointEnums.values()) {
+            listName.add(enpoint.toString());
+        }
+        Specification_Menu menuSpecification = new Specification_Menu();
+        menuSpecification.add(new _SearchCriteria("name", SearchOperator.NOT_IN, listName));
+        List<Menu> menusNotInList = menuRepository.findAll(menuSpecification);
+        menuRepository.deleteAll(menusNotInList);
+        // Create:
+        for (UrlEnpointEnums enpoint : UrlEnpointEnums.values()) {
+            String enpointName = enpoint.toString();
+            String enpointUrl = enpoint.getUrl();
+            String enpointRegex = enpoint.getRegex();
+            String enpointMethod = enpoint.getMethod();
+            MenuRequest menuRequest = new MenuRequest();
+            menuRequest.setName(enpointName);
+            menuRequest.setUrl(enpointUrl);
+            menuRequest.setRegex(enpointRegex);
+            menuRequest.setMethod(enpointMethod);
+            if (!existsByName(enpointName)) {
+                if (enpointName.contains(Constant.MENU_ADDRESS_DEVIDE)) {
+                    String parentName = enpointName.split(Constant.MENU_ADDRESS_DEVIDE)[0];
+                    Menu parentMenu = findByName(parentName);
+                    menuRequest.setParentId(parentMenu.getId());
+                }
+                menuDataValidate.validate(menuRequest);
+                Menu menu = menuRequest.getMenu();
+                save(menu);
+            }
+        }
+    }
 
-	public void deleteMenuHasNameNotInList(List<String> listName) {
-		Specification_Menu menuSpecification = new Specification_Menu();
-		menuSpecification.add(new _SearchCriteria("name", SearchOperator.NOT_IN, listName));
-		List<Menu> menusNotInList = menuRepository.findAll(menuSpecification);
-		menuRepository.deleteAll(menusNotInList);
-	}
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    private class MenuActionsMangement extends ObjectsManagementList<MenuResponse> {
+        MenuActionsMangement(List<MenuResponse> list, Long total) {
+            super(list, total);
+        }
+    }
 }
