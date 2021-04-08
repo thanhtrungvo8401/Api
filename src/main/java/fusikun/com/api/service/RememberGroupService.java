40ea70073d51068d5f93a6f8064b219704ceb97a
trung8401 <thanhtrungvo8401@gmail.com>
2021-04-08 16:03:03 +0700

    007 -- optimize code in RememberGroupController

--- src/main/java/fusikun/com/api/service/RememberGroupService.java
+++ src/main/java/fusikun/com/api/service/RememberGroupService.java
@@ -2,8 +2,16 @@ package fusikun.com.api.service;
 
 
 import fusikun.com.api.dao.RememberGroupRepository;
+import fusikun.com.api.dtoREQ.RememberGroupRequest;
+import fusikun.com.api.dtoRES.ObjectsManagementList;
+import fusikun.com.api.dtoRES.RememberGroupResponse;
+import fusikun.com.api.enums.ApiDataType;
+import fusikun.com.api.enums.SearchOperator;
 import fusikun.com.api.model.study.RememberGroup;
+import fusikun.com.api.specificationSearch.SearchHelpers_RememberGroup;
 import fusikun.com.api.specificationSearch.Specification_RememberGroup;
+import fusikun.com.api.specificationSearch._SearchCriteria;
+import fusikun.com.api.utils.SortHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +19,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -18,6 +27,9 @@ import java.util.*;
 public class RememberGroupService {
     @Autowired
     RememberGroupRepository rememberGroupRepository;
+
+    @Autowired
+    UserService userService;
 
     public RememberGroup save(RememberGroup entity) {
         if (entity.getId() == null) {
@@ -57,4 +69,60 @@ public class RememberGroupService {
         return rememberGroupRepository.findAll(specification);
     }
 
+    // call from controller:
+    public RememberGroupResponse _createRememberGroup(RememberGroupRequest request) {
+        RememberGroup rememberGroup = request.getRemembersGroup();
+        return new RememberGroupResponse(save(rememberGroup));
+    }
+
+    public List<RememberGroupResponse> _findAllByOwnerId(UUID id) {
+        Specification_RememberGroup specification = new Specification_RememberGroup();
+        specification.add(new _SearchCriteria("owner", SearchOperator.EQUAL,
+                userService.findById(id)));
+
+        List<RememberGroup> rememberGroups = findAll(specification);
+        return rememberGroups.stream().map(RememberGroupResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public RemGroupManagement _findManagementByFilters(String filters, String limit, String page,
+                                                       String sortBy, String order) {
+        Specification_RememberGroup specification =
+                new SearchHelpers_RememberGroup(new Specification_RememberGroup(), filters)
+                        .getSpecification(Arrays.asList(
+                                "id," + ApiDataType.UUID_TYPE,
+                                "vocaCodes," + ApiDataType.STRING_TYPE,
+                                "createdDate," + ApiDataType.DATE_TYPE,
+                                "updatedDate," + ApiDataType.DATE_TYPE,
+                                "name," + ApiDataType.DATE_TYPE,
+                                "owner.id," + ApiDataType.UUID_TYPE
+                        ));
+        Pageable pageable = SortHelper.getSort(limit, page, sortBy, order);
+        List<RememberGroup> rememberGroups = findAll(specification, pageable);
+        List<RememberGroupResponse> list =
+                rememberGroups.stream().map(RememberGroupResponse::new)
+                        .collect(Collectors.toList());
+        Long total = count(specification);
+        return new RemGroupManagement(list, total);
+    }
+
+    public RememberGroupResponse _updateById(RememberGroupRequest request, UUID id) {
+        RememberGroup oldRemGroup = findById(id);
+        oldRemGroup.setName(request.getName());
+        oldRemGroup.setVocaCodes(request.getVocaCodes());
+        oldRemGroup.setActiveCodes(request.getActiveCodes());
+        return new RememberGroupResponse(save(oldRemGroup));
+    }
+
+    public RememberGroupResponse _deleteById(UUID id) {
+        RememberGroup rememberGroup = findById(id);
+        delete(rememberGroup);
+        return new RememberGroupResponse(rememberGroup);
+    }
+
+    private class RemGroupManagement extends ObjectsManagementList<RememberGroupResponse> {
+        public RemGroupManagement(List<RememberGroupResponse> list, Long total) {
+            super(list, total);
+        }
+    }
 }
