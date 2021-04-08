b89d936f97207e1e8b9878c2f969c0f93596ed4d
trung8401 <thanhtrungvo8401@gmail.com>
2021-04-08 20:58:22 +0700

    007 -- optimize code in TestGroupController

--- src/main/java/fusikun/com/api/controller/TestGroupController.java
+++ src/main/java/fusikun/com/api/controller/TestGroupController.java
@@ -3,31 +3,19 @@ package fusikun.com.api.controller;
 import fusikun.com.api.dtoRES.ObjectsManagementList;
 import fusikun.com.api.dtoREQ.TestGroupRequest;
 import fusikun.com.api.dtoRES.TestGroupResponse;
-import fusikun.com.api.enums.ApiDataType;
-import fusikun.com.api.enums.SearchOperator;
 import fusikun.com.api.exceptionHandlers.Ex_MethodArgumentNotValidException;
-import fusikun.com.api.model.app.User;
-import fusikun.com.api.model.study.TestGroup;
 import fusikun.com.api.service.TestGroupService;
-import fusikun.com.api.service.UserService;
-import fusikun.com.api.specificationSearch.SearchHelpers_TestGroup;
-import fusikun.com.api.specificationSearch.Specification_TestGroup;
-import fusikun.com.api.specificationSearch._SearchCriteria;
-import fusikun.com.api.utils.SortHelper;
 import fusikun.com.api.validator.TestGroupDataValidate;
 
 import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/common/v1")
@@ -39,41 +27,28 @@ public class TestGroupController {
     @Autowired
     TestGroupService service;
 
-    @Autowired
-    UserService userService;
-
     // Create
     @PostMapping("/test-groups")
     public ResponseEntity<Object> handleCreateTestGroup(
-            @Valid
-            @RequestBody TestGroupRequest req)
+            @Valid @RequestBody TestGroupRequest req)
             throws Ex_MethodArgumentNotValidException {
         dataValidate.validate(req);
-        TestGroup testGroup = req.getTestGroup();
-        service.save(testGroup);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
-                .body(new TestGroupResponse(testGroup));
+                .body(service._createTestGroup(req));
     }
 
     // FETCH TEST_GROUP:
     @GetMapping("/owners/{ownerId}/test-group")
-    public ResponseEntity<Object>
-    handleGetAllTestGroupByOwnerid(@PathVariable UUID ownerId) throws NotFoundException {
-        // validate:
+    public ResponseEntity<List<TestGroupResponse>>
+    handleGetTestGroupsByOwnerId(@PathVariable UUID ownerId)
+            throws NotFoundException {
         dataValidate.validateOwnerNotExistByOwnerId(ownerId);
-        // fetch data:
-        List<TestGroup> testGroups =
-                service.findAll(getTestGroupSpeciByOwner(ownerId));
-        List<TestGroupResponse> testGroupResponses = testGroups
-                .stream()
-                .map(TestGroupResponse::new)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(testGroupResponses);
+        return ResponseEntity.ok(service._getTestGroupsByOwnerId(ownerId));
     }
 
     @GetMapping("/test-group")
-    public ResponseEntity<Object>
+    public ResponseEntity<ObjectsManagementList>
     handleFetchTestGroupsByFilter(
             @RequestParam(name = "filters", required = false) String filters,
             @RequestParam(name = "limit", required = false) String limit,
@@ -81,70 +56,27 @@ public class TestGroupController {
             @RequestParam(name = "sortBy", required = false) String sortBy,
             @RequestParam(name = "order", required = false) String order
     ) {
-        Specification_TestGroup specification =
-                new SearchHelpers_TestGroup(new Specification_TestGroup(), filters)
-                        .getSpecification(Arrays.asList(
-                                "id," + ApiDataType.UUID_TYPE,
-                                "vocaCodes," + ApiDataType.STRING_TYPE,
-                                "createdDate," + ApiDataType.DATE_TYPE,
-                                "updatedDate," + ApiDataType.DATE_TYPE,
-                                "name," + ApiDataType.STRING_TYPE,
-                                "owner.id," + ApiDataType.UUID_TYPE
-                        ));
-        Pageable pageable = SortHelper.getSort(limit, page, sortBy, order);
-        List<TestGroup> testGroups = service.findAll(specification, pageable);
-        Long total = service.count(specification);
-        List<TestGroupResponse> testGroupResponses = testGroups
-                .stream()
-                .map(TestGroupResponse::new)
-                .collect(Collectors.toList());
-        return ResponseEntity.ok(new TestGroupManagement(testGroupResponses, total));
+        return ResponseEntity
+                .ok(service._getTestGroupManagement(filters, limit, page, sortBy, order));
     }
 
     // UPDATE
     @PutMapping("/test-group/{id}")
-    public ResponseEntity<Object> handleUpdateTestGroupById(
+    public ResponseEntity<TestGroupResponse> handleUpdateTestGroupById(
             @PathVariable UUID id,
             @Valid @RequestBody TestGroupRequest req
     ) throws Ex_MethodArgumentNotValidException, NotFoundException {
-        // Validate
         req.setId(id);
         dataValidate.validateTestGroupNotExistById(id);
         dataValidate.validate(req);
-        // Update
-        TestGroup oldTestGroup = service.findById(req.getId());
-        TestGroup testGroup = req.getTestGroup();
-        // ---
-        oldTestGroup.setName(testGroup.getName());
-        oldTestGroup.setVocaCodes(testGroup.getVocaCodes());
-        oldTestGroup.setCorrects(testGroup.getCorrects());
-        service.save(oldTestGroup);
-        return ResponseEntity.ok(new TestGroupResponse(oldTestGroup));
+        return ResponseEntity.ok(service._updateTestGroupById(req, id));
     }
 
     // DELETE
     @DeleteMapping("/test-group/{id}")
-    public ResponseEntity<Object> handleDeleteTestGroupById(
-            @PathVariable UUID id
-    ) throws NotFoundException {
-        // validate
+    public ResponseEntity<Object> handleDeleteTestGroupById(@PathVariable UUID id)
+            throws NotFoundException {
         dataValidate.validateTestGroupNotExistById(id);
-        TestGroup testGroup = service.findById(id);
-        // delete
-        service.delete(testGroup);
-        return ResponseEntity.ok(new TestGroupResponse(testGroup));
-    }
-
-    private Specification_TestGroup getTestGroupSpeciByOwner(UUID ownerId) {
-        Specification_TestGroup specification = new Specification_TestGroup();
-        User owner = userService.findById(ownerId);
-        specification.add(new _SearchCriteria("owner", SearchOperator.EQUAL, owner));
-        return specification;
-    }
-
-    private class TestGroupManagement extends ObjectsManagementList<TestGroupResponse> {
-        public TestGroupManagement(List<TestGroupResponse> list, Long total) {
-            super(list, total);
-        }
+        return ResponseEntity.ok(service._deleteById(id));
     }
 }
--- src/main/java/fusikun/com/api/service/TestGroupService.java
+++ src/main/java/fusikun/com/api/service/TestGroupService.java
@@ -1,8 +1,19 @@
 package fusikun.com.api.service;
 
 import fusikun.com.api.dao.TestGroupRepository;
+import fusikun.com.api.dtoREQ.TestGroupRequest;
+import fusikun.com.api.dtoRES.ObjectsManagementList;
+import fusikun.com.api.dtoRES.TestGroupResponse;
+import fusikun.com.api.enums.ApiDataType;
+import fusikun.com.api.enums.SearchOperator;
 import fusikun.com.api.model.study.TestGroup;
+import fusikun.com.api.specificationSearch.SearchHelpers_TestGroup;
 import fusikun.com.api.specificationSearch.Specification_TestGroup;
+import fusikun.com.api.specificationSearch._SearchCriteria;
+import fusikun.com.api.utils.SortHelper;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,12 +21,16 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(rollbackFor = Exception.class)
 public class TestGroupService {
     @Autowired
     TestGroupRepository repository;
+
+    @Autowired
+    UserService userService;
 
     public TestGroup save(TestGroup entity) {
         if (entity.getId() == null) {
@@ -53,5 +68,72 @@ public class TestGroupService {
 
     public void delete(TestGroup entity) {
         repository.delete(entity);
+    }
+
+    public TestGroupResponse _createTestGroup(TestGroupRequest req) {
+        TestGroup testGroup = req.getTestGroup();
+        return new TestGroupResponse(save(testGroup));
+    }
+
+    public List<TestGroupResponse> _getTestGroupsByOwnerId(UUID id) {
+        Specification_TestGroup specification = new Specification_TestGroup();
+        specification.add(new _SearchCriteria(
+                "owner",
+                SearchOperator.EQUAL,
+                userService.findById(id)
+        ));
+        List<TestGroup> testGroups = findAll(specification);
+        return testGroups.stream()
+                .map(TestGroupResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public TestGroupManagement _getTestGroupManagement(
+            String filters,
+            String limit,
+            String page,
+            String sortBy,
+            String order
+    ) {
+        Specification_TestGroup specification =
+                new SearchHelpers_TestGroup(new Specification_TestGroup(), filters)
+                        .getSpecification(Arrays.asList(
+                                "id," + ApiDataType.UUID_TYPE,
+                                "vocaCodes," + ApiDataType.STRING_TYPE,
+                                "createdDate," + ApiDataType.DATE_TYPE,
+                                "updatedDate," + ApiDataType.DATE_TYPE,
+                                "name," + ApiDataType.STRING_TYPE,
+                                "owner.id," + ApiDataType.UUID_TYPE
+                        ));
+        Pageable pageable = SortHelper.getSort(limit, page, sortBy, order);
+        Long total = count(specification);
+        List<TestGroupResponse> testGroupResponses =
+                findAll(specification, pageable)
+                        .stream().map(TestGroupResponse::new)
+                        .collect(Collectors.toList());
+        return new TestGroupManagement(testGroupResponses, total);
+    }
+
+    public TestGroupResponse _updateTestGroupById(TestGroupRequest req, UUID id) {
+        TestGroup oldTestGroup = findById(id);
+        oldTestGroup.setName(req.getName());
+        oldTestGroup.setVocaCodes(req.getVocaCodes());
+        oldTestGroup.setCorrects(req.getCorrects());
+        return new TestGroupResponse(save(oldTestGroup));
+    }
+
+    public TestGroupResponse _deleteById(UUID id) {
+        TestGroup testGroup = findById(id);
+        delete(testGroup);
+        return new TestGroupResponse(testGroup);
+    }
+
+    @Getter
+    @Setter
+    @NoArgsConstructor
+    private class TestGroupManagement extends ObjectsManagementList<TestGroupResponse> {
+        public TestGroupManagement(List<TestGroupResponse> list, Long total) {
+            super(list, total);
+        }
     }
 }
