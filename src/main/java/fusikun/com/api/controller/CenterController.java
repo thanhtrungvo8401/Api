267c526caf339224bcad4cf88ab60c13b4af1879
trung8401 <thanhtrungvo8401@gmail.com>
2021-04-08 14:39:43 +0700

    007 -- optimize code in CenterController

--- src/main/java/fusikun/com/api/controller/CenterController.java
+++ src/main/java/fusikun/com/api/controller/CenterController.java
@@ -1,16 +1,13 @@
 package fusikun.com.api.controller;
 
 import fusikun.com.api.dtoRES.CenterResponse;
-import fusikun.com.api.model.study.Center;
 import fusikun.com.api.service.CenterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class CenterController {
@@ -19,15 +16,6 @@ public class CenterController {
 
     @GetMapping("/api/v1/centers")
     public ResponseEntity<List<CenterResponse>> handleGetCenters() {
-        List<Center> centers = centerService.findAll();
-        return ResponseEntity.ok(centers.stream().map(CenterResponse::new).collect(Collectors.toList()));
-    }
-
-    @GetMapping("/api/common/v1/centers")
-    public ResponseEntity<CenterResponse> handleGetCenterByCenterName(
-            @RequestParam(name = "centerName") String centerName
-    ) {
-        Center center = centerService.findByCenterName(centerName);
-        return ResponseEntity.ok(new CenterResponse(center));
+        return ResponseEntity.ok(centerService._findAllCenters());
     }
 }
