114f474ced58b436d738f23c65f654aa520e413d
trung8401 <thanhtrungvo8401@gmail.com>
2021-04-10 09:31:40 +0700

    007 -- Handle now throw exception when check token

--- src/main/java/fusikun/com/api/filters/AuthenticateFilter.java
+++ src/main/java/fusikun/com/api/filters/AuthenticateFilter.java
@@ -26,45 +26,56 @@ import fusikun.com.api.utils.JwtTokenUtil;
 @Component
 public class AuthenticateFilter extends OncePerRequestFilter {
 
-	@Autowired
-	private JwtUserDetailsService jwtUserDetailsService;
+    @Autowired
+    private JwtUserDetailsService jwtUserDetailsService;
 
-	@Autowired
-	private JwtTokenUtil jwtTokenUtil;
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
 
-	@Override
-	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
-			throws ServletException, IOException {
-		if (IgnoreUrl.isPublicUrl(request)) {
-			chain.doFilter(request, response);
-			return;
-		}
-		
-		final String authorizationHeader = request.getHeader(Constant.AUTH_AUTHORIZATION);
-		// check exist and valid token format:
-		if (authorizationHeader == null || !authorizationHeader.startsWith(Constant.AUTH_BEARER)) {
-			System.out.println("======= JWT does not start with 'bearer' OR NULL =======");
-			throw new Ex_InvalidTokenException(ConstantMessages.INVALID_TOKEN);
-		}
-		String jwt = authorizationHeader.substring(Constant.AUTH_BEARER_INDEX);
-		UserInfoObject userInfo = jwtTokenUtil.getUserInfoFromToken(jwt);
+    /**
+     * When we call
+     * -chain.doFilter(request, response);
+     * -return;
+     * That mean we go to next step and do not authenticate for request and then request
+     * will be checked by spring-security to block this request.
+     */
 
-		// check valid token
-		if (userInfo == null || userInfo.getId() == null
-				|| SecurityContextHolder.getContext().getAuthentication() != null) {
-			throw new Ex_InvalidTokenException(ConstantMessages.INVALID_TOKEN);
-		}
-		JwtUserDetails userDetails = jwtUserDetailsService.loadUserByUserInfo(userInfo);
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws ServletException, IOException {
+        if (IgnoreUrl.isPublicUrl(request)) {
+            chain.doFilter(request, response);
+            return;
+        }
 
-		// check valid token
-		if (!jwtTokenUtil.validateToken(jwt, userDetails)) {
-			System.out.println("======= invalid TOKEN =======");
-			throw new Ex_InvalidTokenException(ConstantMessages.INVALID_TOKEN);
-		}
-		UsernamePasswordAuthenticationToken object = new UsernamePasswordAuthenticationToken(userDetails, null,
-				userDetails.getAuthorities());
-		object.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-		SecurityContextHolder.getContext().setAuthentication(object);
-		chain.doFilter(request, response);
-	}
+        final String authorizationHeader = request.getHeader(Constant.AUTH_AUTHORIZATION);
+        // check exist and valid token format:
+        if (authorizationHeader == null || !authorizationHeader.startsWith(Constant.AUTH_BEARER)) {
+            System.out.println("======= JWT does not start with 'bearer' OR NULL =======");
+            chain.doFilter(request, response);
+            return;
+        }
+        String jwt = authorizationHeader.substring(Constant.AUTH_BEARER_INDEX);
+        UserInfoObject userInfo = jwtTokenUtil.getUserInfoFromToken(jwt);
+
+        // check valid token
+        if (userInfo == null || userInfo.getId() == null
+                || SecurityContextHolder.getContext().getAuthentication() != null) {
+            throw new Ex_InvalidTokenException(ConstantMessages.INVALID_TOKEN);
+        }
+        JwtUserDetails userDetails = jwtUserDetailsService.loadUserByUserInfo(userInfo);
+
+        // check valid token
+        if (!jwtTokenUtil.validateToken(jwt, userDetails)) {
+            System.out.println("======= invalid TOKEN =======");
+            chain.doFilter(request, response);
+            return;
+        }
+        // START TO AUTHENTICATE FOR REQUEST
+        UsernamePasswordAuthenticationToken object = new UsernamePasswordAuthenticationToken(userDetails, null,
+                userDetails.getAuthorities());
+        object.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(object);
+        chain.doFilter(request, response);
+    }
 }
